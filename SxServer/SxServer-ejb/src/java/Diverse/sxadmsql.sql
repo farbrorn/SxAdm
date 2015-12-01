742b08a77806a8b81ae7159741298ee50ee55e41
Ulf <ulf.hemma@gmail.com>
2015-12-01 21:47:21 +0100

    ändrat i view för butik

--- SxServer/SxServer-ejb/src/java/Diverse/sxadmsql.sql
+++ SxServer/SxServer-ejb/src/java/Diverse/sxadmsql.sql
@@ -2436,7 +2436,7 @@ order by v.kontaktid, g.max_ch_timestamp, akl.sortorder, akl.artnr
 create or replace view vbutikart as
 select  
 ak.klasid as ak_klasid, ak.rubrik as ak_rubrik, ak.text as ak_text, ak.html as ak_html, ak.autosortvikt as ak_autosortvikt,
-akl.sortorder as akl_sortorder,
+akl.sortorder as akl_sortorder, ak.auto_samkopta_klasar as ak_auto_samkopta_klasar,
 a.*,
 lid.lagernr as lid_lagernr, lid.bnamn as lid_bnamn, lid.namn as lid_namn, lid.adr1 as lid_adr1, lid.adr2 as lid_adr2, lid.adr3 as lid_adr3,lid.tel as lid_tel, lid.email as lid_email,
 l.lagernr as l_lagernr, coalesce(l.ilager,0) as l_ilager, coalesce(l.bestpunkt,0) as l_bestpunkt, coalesce(l.maxlager,0) as l_maxlager, coalesce(l.best,0) as l_best, coalesce(l.iorder,0) as l_iorder,
@@ -2531,3 +2531,4 @@ alter table artklase add column auto_sokord varchar;
 alter table artklase add column auto_sokartnr varchar;
 alter table artklase add column auto_sokrefnr varchar;
 alter table artklase add column auto_bildartnr varchar;
+alter table artklase add column auto_samkopta_klasar varchar;
